3eaf0384a26ccfd2f55887f28db53cc122f4c019
romeo <romeo@selas.studio>
2023-08-11 11:33:19 +0200

    fix unittest

--- test/test_dashboard.py
+++ test/test_dashboard.py
@@ -2,7 +2,7 @@ from contextvars import copy_context
 from dash._callback_context import context_value
 from dash._utils import AttributeDict
 
-from source.dashboard import update_graph
+from source.dashboard import update_api
 
 
 def test_update_callback():
